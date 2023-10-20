c4cb1492b7eb9ed016f0c9be9e74fa793cfb7ad7
nadyasri <116337299+nadyasri@users.noreply.github.com>
2023-10-20 16:18:39 +0700

    Add files via upload

---
+++ WAD_PROJECT2/Latihan2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2 PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body>
+<form>
+<h1> FORM PENDATAAN DIRI </h1>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Masukan Angka  </label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+  </div>
+  <br>
+
+<br>
+<select class="form-select" aria-label="Default select example">
+  <option selected>Operasi Bilangan</option>
+  <option value="Tambah">Tambah</option>
+  <option value="Kurang"> Kurang</option>
+</div>
+<br>
+</select>
+  </div>
+  <br>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+ 
+</body>
+</html>
